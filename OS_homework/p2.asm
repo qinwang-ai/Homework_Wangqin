378b511ea622704c523c0803c6f9fa7b8391e559
wangqin4377 <wangqin4377@qq.com>
2015-06-17 13:15:17 +0800

    a

--- OS_homework/p2.asm
+++ OS_homework/p2.asm
@@ -5,7 +5,7 @@ print_str:
 	mov cx,strl
 	mov ax,1301h
 	mov bx,78D
-	mov dx,1220h
+	mov dx,1820h
 	int 10h
 	pop bp
 
@@ -22,6 +22,7 @@ je m_n_r
 jmp loop_k
 m_n_l:
 call moveleft
+jmp loop_k
 m_n_r:
 call moveright
 jmp loop_k
@@ -30,30 +31,47 @@ jmp loop_k
 moveleft:
 	mov ax,cs
 	mov ds,ax
-	mov di,[ now_index]
-	dec di
-	dec di
-	mov [ now_index],di
+	mov si,[ now_index]
+	dec si
+	dec si
+	mov [ now_index],si
 	mov ax,0xb800
 	mov es,ax
-	mov bx,now_index
+	mov bx,[now_index]
 	mov byte [es:bx],'='
 	mov cx,strl
+	add bx,cx
 	add bx,cx
 	mov byte [es:bx],' '
 ret
 	
 
 moveright:
+	mov ax,cs
+	mov ds,ax
+	mov si,[ now_index]
+	inc si
+	inc si
+	mov [ now_index],si
+	mov ax,0xb800
+	mov es,ax
+	mov bx,[now_index]
+	mov byte [es:bx],'='
+	mov cx,strl
+	sub bx,cx
+	sub bx,cx
+	mov byte [es:bx],' '
+ret
 
 
-
-now_index dd 2442D 
+now_index dd 3904D 
 
 str:
 	db '================='
 strl equ $-str
 
+times 510-($-$$) db 0
+db 0x55,0xaa
 
 
 
